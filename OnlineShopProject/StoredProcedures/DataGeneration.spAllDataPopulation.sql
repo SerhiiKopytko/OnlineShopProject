d65cdc885c5de3494b49896c181c9d8d04c0f1c1
Serhii Kopytko <KopytkoSB@gmail.com>
2020-12-04 10:38:36 +0200

    Add stored procedures for data generation cities, customers, products and product types

---
+++ OnlineShopProject/StoredProcedures/DataGeneration.spAllDataPopulation.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [DataGeneration].[spAllDataPopulation]
+AS
+BEGIN
+	EXECUTE DataGeneration.spDataMasterCities
+	EXECUTE DataGeneration.spDataMasterCustomers
+	EXECUTE DataGeneration.spDataMasterProductTypes
+	EXECUTE DataGeneration.spDataMasterProducts
+END
---
+++ OnlineShopProject/StoredProcedures/DataGeneration.spDataMasterProductTypes.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [DataGeneration].[spDataMasterProductTypes]
+AS
+BEGIN
+	IF OBJECT_ID ('[Master].ProductTypes', 'U') IS NOT NULL
+	BEGIN
+		ALTER TABLE [Master].Products
+		DROP CONSTRAINT FK_MasterProductsProductTypeID_MasterProductTypesProductTypeID
+
+			DELETE [Master].ProductTypes
+			DBCC CHECKIDENT ('[Master].ProductTypes', RESEED, 0)
+
+			INSERT INTO [Master].ProductTypes(ProductTypeName, ProductTypeDescription)
+			VALUES
+				('Laptop', 'A laptop (also laptop computer), is a small, portable personal computer (PC)'),
+				('Smartphones', 'A smartphone is a mobile device that combines cellular and mobile computing functions into one unit.')
+
+		ALTER TABLE [Master].Products
+		ADD CONSTRAINT FK_MasterProductsProductTypeID_MasterProductTypesProductTypeID FOREIGN KEY (ProductTypeID) REFERENCES [Master].ProductTypes(ProductTypeID)
+	END
+END
