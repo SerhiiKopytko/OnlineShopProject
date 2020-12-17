e5452e253728865b2f023998c57b9436fc4a9c14
Serhii Kopytko <KopytkoSB@gmail.com>
2020-12-17 06:38:56 +0200

    Add new stored procedures

---
+++ OnlineShopProject/StoredProcedures/DataGeneration/DataGeneration.spRunOneBuying.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE [DataGeneration].[spRunOneBuying]
+AS
+
+BEGIN
+
+DECLARE
+@FirstOrderID INT,
+@LastOrderID INT,
+@CurrentOrderID INT,
+
+@EventProcName VARCHAR(250) = OBJECT_SCHEMA_NAME(@@PROCID)+'.'+OBJECT_NAME(@@PROCID),
+@RowCount INT = 0,
+@RowCountProd INT
+
+	EXECUTE Logs.spStartOperation @EventProcName -- logging start operation process
+
+	SELECT @FirstOrderID = MIN(OrderID), 
+		   @LastOrderID  = MAX(OrderID)
+	FROM Master.Orders
+
+	SET @CurrentOrderID = @FirstOrderID
+
+		WHILE @CurrentOrderID <= @LastOrderID
+			BEGIN
+				EXECUTE @RowCountProd = Master.spBuyingProducts @CurrentOrderID
+
+				SET @RowCount = @RowCount + @RowCountProd -- Calculate and save how many rows were populeted
+			    SET @CurrentOrderID +=1
+			END
+
+EXECUTE Logs.spCompletedOperation @EventProcName, @RowCount	-- Compliting operation process
+END;
---
+++ OnlineShopProject/StoredProcedures/Master.spBuyingProducts.sql
@@ -0,0 +1,61 @@
+﻿CREATE PROCEDURE [Master].[spBuyingProducts]
+(
+@OrderID  INT
+)
+AS
+
+BEGIN
+
+DECLARE
+@StartOrderDetailID INT,
+@EndOrderDetailID INT,
+@CurrentOrderDetailID INT,
+@CurrentWHid INT,
+
+@EventProcName VARCHAR(250) = OBJECT_SCHEMA_NAME(@@PROCID)+'.'+OBJECT_NAME(@@PROCID),
+@RowCount INT = 0
+
+EXECUTE Logs.spStartOperation @EventProcName -- logging start operation process
+
+EXECUTE Master.spCreateNewBuyingVersion @OrderID -- Create a new Version for the current order
+
+-- SELECT 'OrderDetailID' for chosen @OrderID
+	SELECT @StartOrderDetailID = MIN(od.OrderDetailID), 
+		   @EndOrderDetailID   = MAX(od.OrderDetailID)
+	FROM Master.Orders AS o
+	JOIN Master.OrderDetails AS od ON o.OrderID = od.OrderID
+	WHERE o.OrderID = @OrderID
+
+
+	SET @CurrentOrderDetailID = @StartOrderDetailID
+
+	-- selecting a current product from the 'WareHouse' table and update necessary fields
+		WHILE @CurrentOrderDetailID <= @EndOrderDetailID
+			BEGIN
+				SELECT @CurrentWHid = WareHouseID 
+					FROM Master.WareHouses
+					WHERE 1 = 1
+					  AND ProductID = (SELECT ProductID 
+										FROM Master.OrderDetails
+										WHERE OrderDetailID = @CurrentOrderDetailID)
+					  AND EndVersion = 999999999
+					ORDER BY StartVersion, WareHouseID
+					OFFSET 0 ROWS FETCH FIRST 1 ROWS ONLY
+
+					UPDATE Master.WareHouses
+						SET EndVersion = IDENT_CURRENT('Master.VersionConfigs')  
+						WHERE WareHouseID = @CurrentWHid
+
+					UPDATE Master.OrderDetails
+						SET WareHouseID = @CurrentWHid
+						WHERE OrderDetailID = @CurrentOrderDetailID
+
+					SET @RowCount = @RowCount + (SELECT @@ROWCOUNT) -- Calculate and save how many rows were populeted
+
+			  SET @CurrentOrderDetailID +=1 
+			END
+
+	EXECUTE Logs.spCompletedOperation @EventProcName, @RowCount	-- Compliting operation process
+
+	RETURN @RowCount
+END;
