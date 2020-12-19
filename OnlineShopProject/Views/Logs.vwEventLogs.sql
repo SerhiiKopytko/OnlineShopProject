5e08b99d38f70e48220b34ba057e3bc98b4febf0
Serhii Kopytko <KopytkoSB@gmail.com>
2020-12-19 14:36:21 +0200

    added views

---
+++ OnlineShopProject/Views/Logs.vwEventLogs.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [Logs].[vwEventLogs]
+	AS 
+	SELECT e.EventID,
+	   e.OperationRunID,
+	   s.StatusName,
+	   e.AffectedRows,
+	   e.EventProcName,
+	   e.EventMessage,
+	   e.EventDataTime,
+	   e.[User]
+FROM Logs.EventLogs AS e
+JOIN Logs.OperationsStatuses AS s ON s.OperationStatusID = e.EventStatusID
---
+++ OnlineShopProject/Views/Logs.vwOperationRuns.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [Logs].[vwOperationRuns]
+	AS 
+	SELECT r.OperationRunID,
+	   r.ManagerID,
+	   s.StatusName,
+	   r.OperationRunDescription,
+	   r.StartTime,
+	   r.EndTime
+FROM Logs.OperationRuns AS r
+JOIN Logs.OperationsStatuses AS s ON s.OperationStatusID = r.StatusID
