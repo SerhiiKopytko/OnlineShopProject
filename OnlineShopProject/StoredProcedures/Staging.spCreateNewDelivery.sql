043f45a70e77a2dab092abdab46a90537480a412
Serhii Kopytko <KopytkoSB@gmail.com>
2020-12-23 10:54:48 +0200

    add new sp for new delivery

--- OnlineShopProject/StoredProcedures/Staging.spCreateNewDelivery.sql
+++ OnlineShopProject/StoredProcedures/Staging.spCreateNewDelivery.sql
@@ -6,6 +6,7 @@
  @MaxPriceProd INT = 3000,
  @MinAmountProd INT = 3,
  @MaxAmountProd INT = 5
+
 )
 AS
 
@@ -16,7 +17,21 @@ DECLARE
  @CounterKingProd INT,
  @RandomPriceProd INT,
  @RandomAmountProd INT,
- @CounterProd INT
+ @CounterProd INT,
+ @DeliveryDate DATETIME,
+
+	@prevAdvancedOptions INT, -- variables for managing xp_cmdshell Server configuration option
+	@prevXpCmdshell INT,       --variables for managing xp_cmdshell Server configuration option
+
+	@bcp_cmd VARCHAR(1000),
+	@exe_path VARCHAR(200) = 'call "C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\BCP.EXE"',
+	@FromTable VARCHAR(250) = ' OnlineShop.##NewDelivery',
+	@Path VARCHAR(350) = ' C:\TempSSIS\',
+	@FileName VARCHAR(150) = 'NewDelivery.txt',
+	@bcpParam VARCHAR(50) = ' -T -c',
+	@ServerName VARCHAR(150) = ' -S LV575',
+	@UserName VARCHAR(150) = ' -U SOFTSERVE\skopy ',
+	@Delimetr VARCHAR(10) = ' -t ","'
 
 DROP TABLE IF EXISTS #RanProd -- temporary table for storing random unique kind of products for new delivery
 CREATE TABLE #RanProd
@@ -35,12 +50,15 @@ SELECT TOP(@RandomAmountKindProd) ProductID
 
 DROP TABLE IF EXISTS ##NewDelivery -- temporary table for storing all information about new delivery
 CREATE TABLE ##NewDelivery
-(ProductID INT,
+(DeliveryID INT IDENTITY(1,1),
+ ProductID INT,
  PricePerUnit SMALLMONEY,
  DeliveryDate DATETIME)
 
 -- For each unique kind of product creates a random amount of it and  price
 -- and insert this information into 'Staging.NewDeliveries' table
+SET  @DeliveryDate = CURRENT_TIMESTAMP
+
 SET @CounterKingProd = 1
 	WHILE @CounterKingProd <= @RandomAmountKindProd
 		BEGIN
@@ -53,7 +71,7 @@ SET @CounterKingProd = 1
 						INSERT INTO ##NewDelivery (ProductID, PricePerUnit, DeliveryDate)
 						SELECT ProductID AS ProductID,
 							   @RandomPriceProd AS PricePerUnit,
-							   GETDATE() AS DeliveryDate
+							   @DeliveryDate  AS DeliveryDate
 							FROM #RanProd
 							WHERE ID = @CounterKingProd 
 					SET @CounterProd +=1
@@ -61,4 +79,51 @@ SET @CounterKingProd = 1
 
 		SET @CounterKingProd +=1
 		END
+
+
+-- BCP process for loading new delivery from temporary table to txt file.
+
+	--xp_cmdshell Server configuration option
+			
+		SELECT @prevAdvancedOptions = cast(value_in_use as int) from sys.configurations where name = 'show advanced options'
+		SELECT @prevXpCmdshell = cast(value_in_use as int) from sys.configurations where name = 'xp_cmdshell'
+
+			IF (@prevAdvancedOptions = 0)
+			BEGIN
+				exec sp_configure 'show advanced options', 1
+				reconfigure
+			END
+
+			IF (@prevXpCmdshell = 0)
+			BEGIN
+				exec sp_configure 'xp_cmdshell', 1
+				reconfigure
+			END
+
+
+	--	 start main bcp block 
+ 
+			SET @bcp_cmd = @exe_path + @FromTable + ' out' + @Path + @FileName + @bcpParam + @ServerName + @UserName + @Delimetr;
+			EXEC master..xp_cmdshell @bcp_cmd;
+
+	--	 end main bcp block 
+
+
+			SELECT @prevAdvancedOptions = cast(value_in_use as int) from sys.configurations where name = 'show advanced options'
+			SELECT @prevXpCmdshell = cast(value_in_use as int) from sys.configurations where name = 'xp_cmdshell'
+
+
+			IF (@prevXpCmdshell = 1)
+			BEGIN
+				exec sp_configure 'xp_cmdshell', 0
+				reconfigure
+			END
+
+			IF (@prevAdvancedOptions = 1)
+			BEGIN
+				exec sp_configure 'show advanced options', 0
+				reconfigure
+			END
+
+
 END;
