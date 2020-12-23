4e890f78a758b5aa4cd0857cfa08da0a102b3da2
Serhii Kopytko <KopytkoSB@gmail.com>
2020-12-23 16:33:55 +0200

    Add [Master].[spBuyingNewOrderProcessWithSSIS]

---
+++ OnlineShopProject/StoredProcedures/Master.spBuyingNewOrderProcessWithSSIS.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [Master].[spBuyingNewOrderProcessWithSSIS]
+
+AS
+
+BEGIN
+
+	DECLARE
+	  @CurrentOrderID INT
+
+		-- Starting 'OperationRuns' process:
+		-- Creating new OperationRunID and creating new record in 'Logs.OperationEvent' table
+		EXECUTE Logs.spStartOperationRuns 
+
+			EXECUTE Master.spLoadNewOrder  -- Load new order into '[Master].[Orders] and [Master].[OrderDetails]' tables
+		
+		    SET @CurrentOrderID = IDENT_CURRENT('Master.Orders')
+
+			EXECUTE Master.spBuyingProducts @CurrentOrderID
+
+
+		-- Completing 'OperationRuns' process:
+		EXECUTE Logs.spCompletedOperationRuns
+END;
