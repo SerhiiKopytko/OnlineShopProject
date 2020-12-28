837b7cca3955e1814f5c947c40463003904ec197
Serhii Kopytko <KopytkoSB@gmail.com>
2020-12-28 23:43:41 +0200

    Add changes

--- OnlineShopProject/StoredProcedures/Master.spRestockingNewDeliveryProcessWithSSIS.sql
+++ OnlineShopProject/StoredProcedures/Master.spRestockingNewDeliveryProcessWithSSIS.sql
@@ -4,14 +4,14 @@ AS
 
 BEGIN
 DECLARE
-@CurrentOperation INT = 2 --Restocking New Delivery Process
-
+@CurrentOperation INT = 2, --Restocking New Delivery Process
+@CurrentVersion INT
 	-- Starting 'OperationRuns' process:
 	-- Creating new OperationRunID and creating new record in 'Logs.OperationEvent' table
 		EXECUTE Logs.spStartOperationRuns @CurrentOperation
 		
-		EXECUTE Master.spCreateNewLoadVersion
-		EXECUTE Master.spLoadingWarehouse 
+		EXECUTE @CurrentVersion = [Master].spCreateNewLoadVersion
+		EXECUTE Master.spLoadingWarehouse @CurrentVersion
 			   
 	-- Completing 'OperationRuns' process:
 		EXECUTE Logs.spCompletedOperationRuns
