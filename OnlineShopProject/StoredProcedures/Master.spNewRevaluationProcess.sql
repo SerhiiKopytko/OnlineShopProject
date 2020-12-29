f718b9cc000f89c9abafd46030905078eb11e0e3
Serhii Kopytko <KopytkoSB@gmail.com>
2020-12-29 23:49:14 +0200

    Add Master.spNewRevaluationProcess

---
+++ OnlineShopProject/StoredProcedures/Master.spNewRevaluationProcess.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROCEDURE [Master].[spNewRevaluationProcess]
+	@MinProd INT = 1,
+	@MaxProd INT = 2
+AS
+
+BEGIN
+	DECLARE
+		@RandProd INT,
+		@NewPrice SMALLMONEY
+
+
+		SET @RandProd = (SELECT FLOOR(RAND()*(@MaxProd - @MinProd+1))+@MinProd)
+
+		CREATE TABLE #CurrentPriceChanges( 
+			ProductID INT,
+			OldPrice SMALLMONEY,
+			NewPrice SMALLMONEY,
+			OldVersion INT,
+			NewVersion INT,
+			RevaluationDate DATETIME)
+
+		INSERT INTO #CurrentPriceChanges(ProductID, 
+										 OldVersion,
+										 OldPrice)
+			SELECT ProductID    AS ProductID, 
+				   StartVersion AS OldVersion, 
+				   Price        AS OldPrice
+			FROM Master.vwAvailableProd
+			WHERE ProductID IN (SELECT TOP(2) p.ProductID 
+								FROM (SELECT DISTINCT ProductID 
+									  FROM Master.vwAvailableProd) AS p
+									  ORDER BY NEWID()
+								)
+
+	
+		UPDATE #CurrentPriceChanges
+		SET NewPrice = (SELECT CEILING(AVG(p.OldPrice))
+						FROM #CurrentPriceChanges AS p
+						GROUP BY p.ProductID
+						HAVING p.ProductID = #CurrentPriceChanges.ProductID
+						)
+
+
+END;
